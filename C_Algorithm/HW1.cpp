98b27344adc5f3d40b98838e7c312cd13648d879
wlrma0108 <dlwlrma0108@gmail.com>
2022-04-04 18:32:44 +0900

    commit

--- C_Algorithm/HW1.cpp
+++ C_Algorithm/HW1.cpp
@@ -1,9 +1,9 @@
 #include<stdio.h>
-
+#include<cstdio>
 
 typedef struct Listdata {
 	char name[30];
-	int id;
+	int id[400];
 
 }listData;
 
@@ -22,15 +22,58 @@ typedef struct LinkedList {
 
 
 int main(void) {
+	char command[10];
+	struct Listdata D;
+	FILE* f;
+	f = fopen("student.txt", "r");
+	for (int i= 0; i < 15; i++) {
+		fscanf(f, "%s, %d", &D.name, &D.id);
+	}
 
+	fclose(f);
+	
+	printf("명령을 입력하세요");
+	gets(command);
+
+}
+
+void printList(linkedList* L) {
 
 
 
 }
+listNode* search(linkedList* L, int x) {
 
-int delete(linkedList* L, int x) {
 
 
+	return 0;
+}
+int insert(linkedList* L, listNode* pre, listData item) {
+
+
+	return 0;
+
+}
+int delete_list(linkedList* L, int x) {
+
+	
 
 	return 1;
 };
+void reverse(linkedList* L) {
+
+}
+int getLength(linkedList* L) {
+
+
+
+	return 0;
+
+}
+
+int exit() {
+
+
+
+	return 0;
+}
